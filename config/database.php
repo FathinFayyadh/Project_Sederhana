641d3b78af2e6c4ee1f9e459e6610d0e50297efc
Fathin Fayyadh <fathinfayyadh4@gmail.com>
2024-04-15 15:53:31 +0700

    update

--- config/database.php
+++ config/database.php
@@ -1,32 +1,30 @@
 <?php
 session_start();
 
-class Database{
+class Database {
 
-  private  $hostname ="localhost";
-  private  $username="root";
-  private  $password="";
-  private  $database_name="acadmey_ubd";
-  private $sql;
+    private $hostname = "localhost";
+    private $username = "root";
+    private $password = "";  // Masukkan kata sandi Anda di sini
+    private $database_name = "acadmey_ubd";
+    private $sql;
 
-
-  public function __construct()
-  {
-    $this->sql= $this->connectDB();
-  }
-    
-  private function connectDB(){
-
-    $sql= mysqli_connect($this->hostname,$this->password,$this->username,$this->database_name);
-    if(mysqli_connect_errno()){
-        die("Koneksi database Gagal :".mysqli_connect_error());
-
+    public function __construct() {
+        $this->sql = $this->connectDB();
     }
-    return $sql;
-  }
-  public function getConnetion(){
+
+    private function connectDB() {
+        $sql = mysqli_connect($this->hostname, $this->username, $this->password, $this->database_name);
+
+        if (mysqli_connect_errno()) {
+            die("Koneksi database Gagal: " . mysqli_connect_error());
+        }
+
+        return $sql;
+    }
+
+    public function getConnection() {
         return $this->sql;
-  }
-   
+    }
 }
 ?>
--- route.php
+++ route.php
@@ -1,38 +1,45 @@
-<?php 
-$action =isset($_GET['$action'])? $_GET['$action']:'/';
+<?php
+
+require 'config/database.php';
+$nowConnection = new Database();
+
+print_r($nowConnection->getConnection());  
+
+$action = isset($_GET['action']) ? $_GET['action'] : '/';  
 
 switch ($action){
   
-    case'/':
-        echo"index";
+    case '/':
+        echo "index";
         break;
 
-     case'/detail-mahasiswa':
-     echo"Masuk ke detail mahasiswa"; 
-     break;
-     
-     case'/tambah mahasisiwa':
-        echo"masukkan dan tambah mahasiswa";
-       break;
-       
-      case '/proses-tambah-mahasiswa':
-        echo"masuk ke proses Tambah Mahasiswa"; 
+    case '/detail-mahasiswa':
+        echo "Masuk ke detail mahasiswa"; 
         break;
 
-        case'/delete-mahasiswa':
-            echo"masuk keproses  delete";
-            break;
+    case '/tambah-mahasiswa':
+        echo "Masukkan dan tambah mahasiswa";
+        break;
 
-         case'/update-mahasiswa':
-            echo"Masuk keproses update mahasiswa";
-            break;
-          case'/proses update mahasiswa':  
-            echo"Masuk keproses update mahasiswa";
-            break;
-            default:
-            echo"404 Not Found";
-            break;
+    case '/proses-tambah-mahasiswa':
+        echo "Masuk ke proses Tambah Mahasiswa"; 
+        break;
 
-};
+    case '/delete-mahasiswa':
+        echo "Masuk ke proses delete";
+        break;
+
+    case '/update-mahasiswa':
+        echo "Masuk ke proses update mahasiswa";
+        break;
+
+    case '/proses-update-mahasiswa': 
+        echo "Masuk ke proses update mahasiswa";
+        break;
+
+    default:
+        echo "404 Not Found";
+        break;
+}
 
 ?>
